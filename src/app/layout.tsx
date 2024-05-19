511fe017e5bc45b9dd1917102ea1e23f88a27fcd
balckowl <shiggyjr1492@gmail.com>
2024-05-19 11:30:20 +0900

    add: GAを追加しました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer";
+import GoogleAnalytics from "./components/GoogleAnalytics/GoogleAnalytics";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
@@ -12,12 +13,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body className={notoSansJP.className}>
-          <Header />
-          <main>
-            {children}
-          </main>
-          <Footer />
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
