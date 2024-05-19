c68bd8e4a4cb86c4ed04058cda7e40e079bb541d
balckowl <shiggyjr1492@gmail.com>
2024-05-19 11:44:48 +0900

    fix: gaのライブラリを導入しました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer";
-import GoogleAnalytics from "./components/GoogleAnalytics/GoogleAnalytics";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
@@ -13,9 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head>
-        <GoogleAnalytics />
-      </head>
+      {!!process.env.NEXT_PUBLIC_GA_ID && (
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID} />
+      )}
       <body className={notoSansJP.className}>
         <Header />
         <main>
